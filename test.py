a432bf389bc12c43e966511f5283d8e4c53f5251
mc4lam <lammatthew9@gmail.com>
2021-04-06 11:37:56 -0400

    cleaned up project file

--- test.py
+++ test.py
@@ -21,7 +21,6 @@ keyL = ["right", "left", "q"]
 trial = 0
 
 
-
 def startExperiment():
     instructions = ['This is a test', 
     'you will now see a series of words',
@@ -45,33 +44,3 @@ def startExperiment():
     visual.TextStim(win=mywin, text='End Of Experiment', pos=[0,0], wrapWidth=500).draw()
     mywin.flip()
     core.wait(5.0)
-    
-def practice(num,wordlist):
-    print(f"\nprinting info for Practice {num}: \n")
-    visual.TextStim(win=mywin, text='Practice '+str(num), pos=[0,0]).draw()
-    mywin.flip()
-    core.wait(1)
-    for i in wordlist:
-        visual.TextStim(win=mywin, text=i, pos=[0,0]).draw()
-        mywin.flip()
-        recordTime(i, timer.getTime())
-        mywin.flip()
-        core.wait(0.1)
-    visual.TextStim(win=mywin, text='End of practice '+str(num), pos=[0,0]).draw()
-    mywin.flip()
-    core.wait(1)
-    
-def recordTime(word, currentTime):
-    thisResp=None
-    while thisResp==None:
-        allKeys=event.waitKeys()
-        for thisKey in allKeys:
-            if thisKey in ['right','left']:
-                newTime = timer.getTime()
-                reaction_time = newTime - currentTime 
-                print(f"Current Word: {word} {thisKey} was pressed reaction time is {reaction_time}")
-                thisResp = thisKey
-
-
-
-startExperiment()
